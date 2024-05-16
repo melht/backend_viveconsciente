e47641ac9470b6692984b2f9515067d5d019d0c0
Melanie <meltortolero@gmail.com>
2024-05-16 02:20:35 -0600

    Version 1

---
+++ models/Usuario.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const UsuarioSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  telefono: { type: String },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: false },
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
